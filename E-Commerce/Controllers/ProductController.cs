f004e7efe49e2c7afc81277496f2eed109f8a4e8
FarragAbuelela <farrag.abuelela@gmail.com>
2023-03-29 00:30:25 +0200

    add Product Controller add (AddClothesProduct view) add (get all products view)

---
+++ E-Commerce/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddClothesProduct()
+        {
+            return View();
+        }
+    }
+}
